6c7aa7441c5e02ac65b52f669309356b91a90ff8
Michal Lesniak <michalles1@interia.pl>
2023-03-14 23:47:22 +0100

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  items = [2015,2017,2018,2021,2022];
+}
