68c50a50086c0e6f5b5a186550679662db250bdc
Michal Lesniak <michalles1@interia.pl>
2023-03-22 21:58:05 +0100

    Added add category properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatTableModule} from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,7 @@ import { DialogComponent } from './components/home/dialog/dialog.component';
 import { MatIconModule } from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import { NewIntegrationComponent } from './components/new-integration/new-integration.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +25,9 @@ import { NewIntegrationComponent } from './components/new-integration/new-integr
     AppRoutingModule,
     MatDialogModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule, 
+    MatTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/new-integration/new-integration.component.ts
+++ src/app/components/new-integration/new-integration.component.ts
@@ -1,5 +1,27 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
+
+export interface expense {
+  category: string;
+  amount: number;
+  percent: number;
+}
+
+const EXPENSES: expense[] = [
+  { category: 'Hotel', amount: 10000, percent: 10 },
+  { category: 'Transport', amount: 3000, percent: 3 },
+  { category: 'Hotel', amount: 5000, percent: 5 },
+  
+  { category: 'Hotel', amount: 10000, percent: 10 },
+  { category: 'Transport', amount: 3000, percent: 3 },
+  { category: 'Hotel', amount: 5000, percent: 5 },
+  
+  { category: 'Hotel', amount: 10000, percent: 10 },
+  { category: 'Transport', amount: 3000, percent: 3 },
+  { category: 'Hotel', amount: 5000, percent: 5 },
+  
+];
 
 @Component({
   selector: 'app-new-integration',
@@ -7,6 +29,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-integration.component.css']
 })
 export class NewIntegrationComponent {
+  nameBudget?:string;
+  amountBudget?:number;
+  percentBudget?:number;
+  nameCategory?:string;
+  amountCategory?:number;
+  percentCategory?:number;
+  showAddingCategory?:boolean = true;
+  displayedColums: string[] = ['Category', 'Amount', 'Percent'];
+  dataSource = EXPENSES;
 
 
+  addCategory = () => {
+    
+  };
 }
